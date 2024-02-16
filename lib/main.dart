2a5d21c4f689115500c775a3b3b9a57d9b34e0de
Riyan-Dev <riyan20riyan@icloud.com>
2024-02-16 17:10:52 +0500

    small fix

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
           // This works for code too, not just values: Most code changes can be
           // tested with just a hot reload.
           colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF008A70)),
-          scaffoldBackgroundColor: colorScheme.background,
+          scaffoldBackgroundColor: Colors.white,
           useMaterial3: true,
         ),
         navigatorKey: appNavigationKey,
